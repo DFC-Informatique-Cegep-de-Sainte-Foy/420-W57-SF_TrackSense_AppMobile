0a3dc26bba9a051f0fe58ba7af69eca16f62ebd9
Remi St-Pierre <remistp33@gmail.com>
2024-03-24 19:18:54 -0400

    Second page (testing data)

---
+++ TrackSense/TrackSense/Views/SavedRidesPage.xaml.cs
@@ -0,0 +1,42 @@
+using System.ComponentModel;
+using TrackSense.ViewModels;
+
+namespace TrackSense.Views
+{
+    public partial class SavedRidesPage : ContentPage
+    {
+        //readonly Animation animation;
+        public SavedRidesPage()
+        {
+            InitializeComponent();
+            /*
+            double screenWidth = DeviceDisplay.MainDisplayInfo.Width / DeviceDisplay.MainDisplayInfo.Density;
+            animation = new Animation(v => receptionImg.TranslationX = v,
+                -40, screenWidth, Easing.SinIn);
+
+            BindingContext = viewModel;
+
+            viewModel.PropertyChanged += viewModel_PropertyChanged;*/
+        }
+        /*
+        private void viewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (BindingContext is CompletedRidesViewModel viewModel)
+            {
+                if (e.PropertyName == nameof(viewModel.IsReceivingData))
+                {
+                    if (viewModel.IsReceivingData)
+                    {
+                        animation.Commit(this, "animate", 16, 2500, Easing.SinIn,
+                            (v, c) => receptionImg.TranslationX = -40, () => true);
+                    }
+                    else
+                    {
+                        this.AbortAnimation("animate");
+                    }
+                }
+            }
+        }
+        */
+    }
+}
