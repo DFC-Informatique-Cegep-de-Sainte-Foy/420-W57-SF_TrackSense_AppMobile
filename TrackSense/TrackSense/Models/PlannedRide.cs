5aa279eec5695f8e181515548d33594169c8fcf5
Remi St-Pierre <2295837@csfoy.ca>
2024-03-28 17:19:47 -0400

    Ajout Model PlannedRide

---
+++ TrackSense/TrackSense/Models/PlannedRide.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackSense.Models
+{
+    public class PlannedRide
+    {
+        public Guid PlannedRideId { get; set; }
+        public Guid PlannedRideName { get; set; }
+        public List<PlannedRidePoint> PlannedRidePoints { get; set; }
+        //public PlannedRideStatistics Statistics { get; set; }
+        public PlannedRide(Entities.PlannedRide entite)
+        {
+            if (entite is null)
+            {
+                throw new ArgumentNullException(nameof(entite));
+            }
+
+            this.PlannedRideId = entite.PlannedRideId;
+            this.PlannedRidePoints = entite.PlannedRidePoints.Select(entite => new PlannedRidePoint(entite)).ToList();
+            //this.Statistics = new PlannedRideStatistics(entite.Statistics);
+        }
+    }
+}
