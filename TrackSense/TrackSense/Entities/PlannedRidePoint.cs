77120ac6096b178ee26fb97d136fedc0d640f6da
Remi St-Pierre <2295837@csfoy.ca>
2024-03-28 17:16:43 -0400

    Ajout entite PlannedRidePoint

---
+++ TrackSense/TrackSense/Entities/PlannedRidePoint.cs
@@ -0,0 +1,23 @@
+﻿namespace TrackSense.Entities
+{
+    public class PlannedRidePoint
+    {
+        public int RideStep { get; set; }
+        public Location Location { get; set; }
+        public double? Temperature { get; set; }
+        public TimeSpan? EffectiveTime { get; set; }
+
+        public PlannedRidePoint(int rideStep, Location location, double? temperature, TimeSpan? effectiveTime)
+        {
+            RideStep = rideStep;
+            Location = location;
+            Temperature = temperature;
+            EffectiveTime = effectiveTime;
+        }
+
+        public PlannedRidePoint()
+        {
+            ;
+        }
+    }
+}
