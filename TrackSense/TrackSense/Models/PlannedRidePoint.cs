a706d9b83711e53ff602b6523d077d47dc33ed3d
Remi St-Pierre <2295837@csfoy.ca>
2024-03-28 17:20:23 -0400

    Ajout Model PlannedRidePoint

---
+++ TrackSense/TrackSense/Models/PlannedRidePoint.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackSense.Models
+{
+    public class PlannedRidePoint
+    {
+        public int RideStep { get; set; }
+        public Location Location { get; set; }
+        public double? Temperature { get; set; }
+        public TimeSpan? EffectiveTime { get; set; }
+
+        public PlannedRidePoint(int rideStep, Location location, double temperature, TimeSpan effectiveTime)
+        {
+            RideStep = rideStep;
+            Location = location;
+            Temperature = temperature;
+            EffectiveTime = effectiveTime;
+        }
+        public PlannedRidePoint()
+        {
+            ;
+        }
+
+        public PlannedRidePoint(TrackSense.Entities.PlannedRidePoint entite)
+        {
+            RideStep = entite.RideStep;
+            Location = entite.Location;
+            Temperature = entite.Temperature;
+            EffectiveTime = entite.EffectiveTime;
+        }
+    }
+}
