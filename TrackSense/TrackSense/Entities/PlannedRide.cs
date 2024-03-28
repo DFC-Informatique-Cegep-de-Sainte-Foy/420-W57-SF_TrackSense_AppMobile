64d111c003ad77b37b8431ce89ee15d5b87dd22c
Remi St-Pierre <2295837@csfoy.ca>
2024-03-28 17:15:48 -0400

    Ajout entite PlannedRide

---
+++ TrackSense/TrackSense/Entities/PlannedRide.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackSense.Entities
+{
+    public class PlannedRide
+    {
+        public string UserLogin { get; set; } = null!;
+        public Guid PlannedRideId { get; set; } = Guid.Empty!;
+        public List<PlannedRidePoint> PlannedRidePoints { get; set; } = null!;
+        //public PlannedRideStatistics? Statistics { get; set; } = null;
+    }
+}
